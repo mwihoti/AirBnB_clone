ba2f8f7f7d996f8d567d92153ec66782aafdd2b3
mwihoti <danielmwihoti@gmail.com>
2024-03-08 10:46:53 -0800

    user.py console.py(update, destroy)

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Defines user class"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """Defines class User that inherits from BaseModel"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
