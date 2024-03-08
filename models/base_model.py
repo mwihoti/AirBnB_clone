33e919bf1bebe73d524300eaf79b0aa845da4e37
mwihoti <danielmwihoti@gmail.com>
2024-03-07 21:22:17 -0800

    py basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -21,14 +21,15 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
+
         else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at",
-                                                    "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at",
-                                                    "%Y-%m-dT%H:%M:%S.%f")
-            for m, n in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, m, n)
+
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at", "
+                                                            % Y-%m-% dT % H:% M:%S.%f ")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at", " %Y-%m-dT%H:%M:%S.%f ")
+                for m, n in kwargs.items():
+                    if "__class__" not in key:
+                        setattr(self, m, n)
 
     def __str__(self):
         """Returns string representatins """
@@ -47,7 +48,4 @@ class BaseModel:
         updates the public instance attribute updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
-
-
-    
+                        models.storage.save()
