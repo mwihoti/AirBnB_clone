9dd1b64734246ad082b8ca7c1c659abfe8667981
mwihoti <danielmwihoti@gmail.com>
2024-03-07 19:29:58 -0800

    models & test_models

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,17 @@
+import unittest
+"""
+Tests for class BaseModel
+"""
+import unittest
+from models.base_model import BaseModel
+from io import StringIO
+import sys
+import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Defines class TestBaseodel
+    """
+
+
